a33feb6eab694270f5263f7f7dfb2fb157fe02a0
jooyeon <kohouma21@sps-prosek.cz>
2023-02-22 12:19:40 +0100

    adsaadnibbzaszdbaiufsd

--- main.py
+++ main.py
@@ -1,8 +1,93 @@
+import random
 from tkinter import *
 from PIL import ImageTk, Image
 
 
+class Burger:
+    BUN_MAP = {
+        'Standart bun': 'standart',
+        'Sesame bun': 'sesame',
+        'English muffin': 'english'
+    }
+
+    FILL_MAP = {
+        'Lettuce': 'lettuce',
+        'Tomato': 'tomato',
+        'Bacon': 'bacon',
+        'Chicken patty': 'chicken',
+        'Beef - Rare': 'beefr',
+        'Beef - Medium': 'beefm',
+        'Beef - Well Done': 'beefw'
+    }
+
+    buns = [bun for bun in BUN_MAP]
+
+    def __init__(self):
+        self.window = None
+        self.frame = None
+        self.bun = random.choice(self.buns)
+        self.menu = 'bun'
+
+        self.toppings = ['Lettuce', 'Tomato', 'Lettuce']
+        self.create_window()
+        self.show(self.bun, self.toppings)
+
+    def create_window(self):
+        self.window = Tk()
+        self.window.minsize(500, 200)
+
+        self.frame = Frame(self.window)
+
+    def clear_window(self):
+        for widget in self.window.winfo_children():
+            widget.destroy()
+
+    def reset_window(self):
+        self.clear_window()
+        self.show(self.bun, self.toppings)
+
+    def show(self, bun, fillings):
+        self.create_menu()
+
+        top = bun_pos(self.BUN_MAP[bun], 't')
+        top_img = create_image(image(top))
+        create_img_label(top_img, self.window)
+
+        i = 0
+        fill_imgs = []
+        for filling in fillings:
+            fill_imgs.append(create_image(image(self.FILL_MAP[filling])))
+            create_img_label(fill_imgs[i], self.window)
+            i += 1
+
+        bot = bun_pos(self.BUN_MAP[bun], 'b')
+        bot_img = create_image(image(bot))
+        create_img_label(bot_img, self.window)
+
+        self.window.mainloop()
+
+    def bun_menu(self):
+        selected_bun = StringVar(self.window)
+        selected_bun.set(self.bun)
+
+        bun_menu = OptionMenu(self.window, selected_bun, *self.buns, command=self.change_bun)
+        bun_menu.pack()
+
+    def create_menu(self):
+        if self.menu == 'bun':
+            self.bun_menu()
+            return
+
+        if self.menu == 'topping':
+            pass
+
+    def change_bun(self, bun):
+        self.bun = bun
+        self.reset_window()
+
+
 def option_change(val):
+    root.destroy()
     show_burger(['Lettuce', 'Tomato', 'Lettuce'], fill_map, val, bun_map, root)
 
 
@@ -34,7 +119,6 @@ def show_burger(fillings, possible_fillings: dict, bun, possible_buns: dict, tkw
     i = 0
     fill_imgs = []
     for filling in fillings:
-
         fill_imgs.append(create_image(image(possible_fillings[filling])))
         create_img_label(fill_imgs[i], tkwindow)
         i += 1
@@ -46,32 +130,17 @@ def show_burger(fillings, possible_fillings: dict, bun, possible_buns: dict, tkw
     tkwindow.mainloop()
 
 
-bun_map = {
-    'Standart bun': 'standart',
-    'Sesame bun': 'sesame',
-    'English muffin': 'english'
-}
-
-fill_map = {
-    'Lettuce': 'lettuce',
-    'Tomato': 'tomato',
-    'Bacon': 'bacon',
-    'Chicken patty': 'chicken',
-    'Beef - Rare': 'beefr',
-    'Beef - Medium': 'beefm',
-    'Beef - Well Done': 'beefw'
-}
-
-root = Tk()
-root.minsize(500, 200)
-
-buns = ['Standart bun', 'Sesame bun', 'English muffin']
-selected_bun = StringVar(root)
-selected_bun.set('Standard bun')
-
-bun_menu = OptionMenu(root, selected_bun, *buns, command=option_change)
-bun_menu.pack()
-
-show_burger(['Lettuce', 'Tomato', 'Lettuce'], fill_map, 'Standart bun', bun_map, root)
-
+# root = Tk()
+# root.minsize(500, 200)
+#
+# buns = ['Standart bun', 'Sesame bun', 'English muffin']
+# selected_bun = StringVar(root)
+# selected_bun.set('Standard bun')
+#
+# bun_menu = OptionMenu(root, selected_bun, *buns, command=option_change)
+# bun_menu.pack()
+#
+# show_burger(['Lettuce', 'Tomato', 'Lettuce'], fill_map, 'Standart bun', bun_map, root)
+#
 # root.mainloop()
+burger = Burger()
